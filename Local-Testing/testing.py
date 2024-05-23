aa17ff1a7a4fc40c7baeb158793dc6fde6aa4ee6
Richard Kwong <rkwong792@gmail.com>
2024-05-22 17:51:56 -0700

    Add local testing for use cases & debugging

---
+++ Local-Testing/testing.py
@@ -0,0 +1,42 @@
+# Insert class definition here:
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        # Sort the input array so the problem becomes a problem we solved from Two Sum II using two pointers.
+
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    # r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                        
+        return res
+
+
+# Initialize class here
+myObj = Solution()
+
+# Test Cases
+nums1 = [-1,0,1,2,-1,-4]
+nums2 = [0,1,1]
+nums3 = [0,0,0]
+
+print(myObj.threeSum(nums1))
