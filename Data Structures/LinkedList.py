354362703f595bc9652ed621f115df6296a47657
Richard Kwong <rkwong792@gmail.com>
2024-12-03 12:23:40 -0800

    LinkedList data structure

--- Data Structures/LinkedList.py
+++ Data Structures/LinkedList.py
@@ -1,7 +1,24 @@
+# Sample class
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+# Create list1 and list2 singly linked lists
+list1 = ListNode(1)
+list1.next = ListNode(2)
+list1.next.next = ListNode(4)
+
+list2 = ListNode(1)
+list2.next = ListNode(3)
+list2.next.next = ListNode(4)
+
 # Helper function to print the linked list
 def printLinkedList(node):
     values = []
     while node:
-        values.append(node.val)
+        values.append(str(node.val))
         node = node.next
-    print(" -> ".join(map(str, values)))
+    print(" -> ".join(values))
+
+printLinkedList(list1)
