b2a37822aa5fa2652b7049b7619881703cec369a
Richard Kwong <rkwong792@gmail.com>
2025-03-08 22:17:41 -0800

    Tree.py

---
+++ Data Structures/Tree.py
@@ -0,0 +1,14 @@
+# Tree Problems Notes
+
+'''
+
+ A lot of recursion problems that use DFS has O(N) time complexity where N is the # of nodes in the tree.
+ These recursion problems that use DFS has a O(h) space complexity because of the recursive call stack.
+
+ DFS problems use pre-order traversal a lot (Root -> Left -> Right)
+
+ If you are doing breadth first search (AKA level order traversal), you need to use a queue FIFO data structure.
+ In Python, this is a deque (double ended queue).
+ 
+'''
+
